840257c04ceef077fa7369a9ce60eeda4ce44c2e
Jamie Owen <hello@jamieowen.com>
2021-04-21 21:21:39 +0100

    wip export

--- scripts/export.ts
+++ scripts/export.ts
@@ -1,19 +1,59 @@
-console.log("ookok");
-
+import * as prettier from 'prettier';
 import { GLSLVersion, targetGLSL } from "@thi.ng/shader-ast-glsl";
-// import { BLEND_MODES_3, BLEND_MODES_4, BlendModeDef3 } from "../src";
-import { BlendModeDef3 } from "../src/api";
-import { BLEND_MODES_3 } from "../src/constants";
+import { BLEND_MODES_3, BLEND_MODES_4, BlendModeDef3, blendModeSelect3, blendModeSelect4 } from "../ast";
 
-const compileBlend3 = (blend: BlendModeDef3) => {
+
+const compileBlendModes = ()=>{
+
   const compile = targetGLSL({
-    type: "vs",
+    type: "fs",
     versionPragma: false,
     prelude: "",
     version: GLSLVersion.GLES_100,
   });
-  const str = compile(blend);
-  console.log(str);
-};
 
-Object.entries(BLEND_MODES_3).map(([_key, blend]) => compileBlend3(blend));
+  return {
+    blend3: Object.entries(BLEND_MODES_3).map(([key,fn])=>({ key, src: compile(fn)})),
+    blend4: Object.entries(BLEND_MODES_4).map(([key,fn])=>({ key, src: compile(fn)})),
+    blend3All: compile(blendModeSelect3),
+    blend4All: compile(blendModeSelect4)
+  }
+
+}
+
+// const compileBlend3 = (blend: BlendModeDef3) => {
+Promise.resolve(compileBlendModes()).then(async (res)=>{
+  console.log( 'Await', res );
+  
+})
+  // console.log(blend);
+  // blend.args[0].id = "base";
+  // blend.args[1].id = "blend";
+  // blend.args[2].id = "opacity";
+  // const str = compile(blend);
+  // console.log(str);
+  // const form = prettier.format(str,{
+  //   parser: 'babel'
+  // });
+  // console.log( form );
+// };
+
+
+// const composeGlslify = ( src:string, fnName:string )=>{
+//   return `
+//   ${src}
+//   #pragma glslify: export(${fnName});
+//   `;
+// }
+
+// const composeEsmLiteral = ( src:string )
+
+// Promise.resolve( async ()=>{
+//   console.log( 'okok' );
+// });
+
+// Object.entries(BLEND_MODES_3).map(([_key, blend]) => compileBlend3(blend));
+
+// console.log( 'Prettier' ,prettier.format('var le', {
+//   parser:'babel'
+// }))
